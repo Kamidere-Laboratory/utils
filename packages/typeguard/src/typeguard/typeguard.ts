e5ae532d2df4b02a8ce344a6110fb789639ff288
Robert 'RunAge' Gajda <335453-RunAge@users.noreply.gitlab.com>
2022-04-23 03:04:07 +0200

    refactor(typeguard): change generic type for TypeGuard
    Remove extending ZodSchema in TypeGuard, now accepting infered type for better readability
    
    BREAKING CHANGE: Change TypeGuard gerenic type of ZodSchema to infered type from ZodSchema

--- packages/typeguard/src/typeguard/typeguard.ts
+++ packages/typeguard/src/typeguard/typeguard.ts
@@ -1,11 +1,11 @@
-import type { infer as ZodInfer, ZodIssue, ZodSchema } from "zod";
+import type { ZodSchema } from "zod";
 
 import { TypeGuardAssertionError } from "../errors/typeguard-assert-error.js";
 
-export class TypeGuard<T extends ZodSchema> {
-  constructor(public readonly schema: T) {}
+export class TypeGuard<T> {
+  constructor(public readonly schema: ZodSchema) {}
 
-  assert(value: unknown): asserts value is ZodInfer<T> {
+  assert(value: unknown): asserts value is T {
     const parserd = this.schema.safeParse(value);
     if (parserd.success) {
       return;
@@ -14,7 +14,7 @@ export class TypeGuard<T extends ZodSchema> {
     throw new TypeGuardAssertionError(parserd.error);
   }
 
-  is(value: unknown): value is ZodInfer<T> {
+  is(value: unknown): value is T {
     const parsed = this.schema.safeParse(value);
     return parsed.success;
   }
